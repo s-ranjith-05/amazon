da36773a8806397a78ba41bc0966523b94150413
s-ranjith-05 <deepakranjith2005@gmail.com>
2024-08-19 20:39:49 +0530

    Launch: Files are added.

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+//to load side-navbar.
+var sideBar=document.querySelector(".side-bar");
+var hamburgerBtn=document.querySelector(".hamburger-btn");
+
+hamburgerBtn.addEventListener("click",(event)=>{
+    event.preventDefault();
+    sideBar.style.top="45px";
+})
