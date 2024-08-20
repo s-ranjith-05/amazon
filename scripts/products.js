840c055646c3ca281f375b58f5d7120387493dc5
s-ranjith-05 <deepakranjith2005@gmail.com>
2024-08-20 06:58:20 +0530

    added

---
+++ scripts/products.js
@@ -0,0 +1,58 @@
+//to add products.
+
+const products=[
+    {
+        image:'images/6-piece-non-stick-baking-set.webp',
+        name:'6-Piece Nonstick, Carbon <br>Steel Oven Bakeware Baking',
+        rating:{
+            stars:'images/rating/rating-45.png',
+            people:'87'
+        },
+        cost:3499,
+    },
+    {
+        image:'images/intermediate-composite-basketball.jpg',
+        name:'Intermediate Size Basketball',
+        rating:{
+            stars:'images/rating/rating-4.png',
+            people:'137'
+        },
+        cost:2095,   
+    },
+    {
+        image:'images/adults-plain-cotton-tshirt-2-pack-teal.jpg',
+        name:'Adults Plain Cotton T-Shirt -<br>2 Pack',
+        rating:{
+            stars:'images/rating/rating-45.png',
+            people:'127'
+        },
+        cost:799,   
+    },
+    {
+        image:'images/black-2-slot-toaster.jpg',
+        name:'2 Slot Toaster - Black',
+        rating:{
+            stars:'images/rating/rating-5.png',
+            people:'1297'
+        },
+        cost:1899,   
+    },
+    {
+        image:'images/6-piece-white-dinner-plate-set.jpg',
+        name:'6 Piece White Dinner Plate<br> Set',
+        rating:{
+            stars:'images/rating/rating-4.png',
+            people:'37'
+        },
+        cost:2067,   
+    },
+    {
+        image:'images/athletic-cotton-socks-6-pairs.jpg',
+        name:'Black and Gray Athletic <br>Cotton Socks - 6 Pairs',
+        rating:{
+            stars:'images/rating/rating-45.png',
+            people:'87'
+        },
+        cost:1090,   
+    },
+];
--- scripts/script.js
+++ scripts/script.js
@@ -6,3 +6,34 @@ hamburgerBtn.addEventListener("click",(event)=>{
     event.preventDefault();
     sideBar.style.top="50px";
 })
+
+var productSection=document.querySelector(".pdt-section");
+var html='';
+products.forEach((product)=>{
+     html += `<div class="product">
+            <div class="pdt-image">
+                <img src="${product.image}" alt="">
+            </div>
+            <div class="pdt-details">
+                <p class="pdtname">${product.name}</p>
+                <div class="rating-start">
+                <div class="rating">
+                    <img src="${product.rating.stars}" alt="">
+                    <p><a href="">${product.rating.people}</a></p>
+                </div>
+                <p class="pdt-cost">$ ${((product.cost)/100).toFixed(2)}</p>
+                <div class="select-qty">
+                <select name="" id="">
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                </select></div>
+                <button>Add To Cart</button>
+            </div></div>
+        </div>`;
+});
+
+productSection.innerHTML=html;
+
